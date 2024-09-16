aaca7cbdff14d3565f009cbcc7f787dee68499c8
Qzimyion <samyakchauhan676@gmail.com>
2024-09-17 00:51:29 +0530

    Modified fabric init class

--- fabric/src/main/java/net/github/io/qzimyion/fabric/ExampleModFabric.java
+++ fabric/src/main/java/net/github/io/qzimyion/fabric/ExampleModFabric.java
@@ -2,16 +2,25 @@ package net.github.io.qzimyion.fabric;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.github.io.qzimyion.events.ModServerSideEvents;
 import net.github.io.qzimyion.fabriclike.ExampleModFabricLike;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
 
 public final class ExampleModFabric implements ModInitializer {
     @Override
     public void onInitialize() {
-        // This code runs as soon as Minecraft is in a mod-load-ready state.
-        // However, some things (like resources) may still be uninitialized.
-        // Proceed with mild caution.
-
-        // Run the Fabric-like setup.
         ExampleModFabricLike.init();
+
+        //Events
+        UseBlockCallback.EVENT.register(ExampleModFabric::onRightClickBlock);
+
+    }
+    public static InteractionResult onRightClickBlock(Player player, Level level, InteractionHand hand, BlockHitResult hitResult) {
+        return ModServerSideEvents.onBlockCLicked(player.getItemInHand(hand), player, level, hand, hitResult);
     }
 }
