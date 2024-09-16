9855d1d493de97d27d5fb49a692dd984531df8dc
Qzimyion <samyakchauhan676@gmail.com>
2024-09-17 00:47:41 +0530

    Restructuring the init common class

--- common/src/main/java/net/github/io/qzimyion/VanillaRethoughtCommon.java
+++ common/src/main/java/net/github/io/qzimyion/VanillaRethoughtCommon.java
@@ -1,18 +1,18 @@
 package net.github.io.qzimyion;
 
+import eu.midnightdust.lib.config.MidnightConfig;
 import net.github.io.qzimyion.registry.RegBlocks;
+import net.github.io.qzimyion.registry.RegConfig;
+import net.github.io.qzimyion.registry.RegCreativeTabs;
 
 public final class VanillaRethoughtCommon {
     public static final String MOD_ID = "vanilla_rethought";
 
-    /** We would write and initialize most of our code in common subproject.
-     If we want to use any of the very loader specific stuff use the {@code @ExpectPlatform} annotation
-     and then return a new Assertion error in common
-     And then later create the same class and directory in the other loader subprojects
-     And then implement the loader specific methods
-     */
-
     public static void init() {
+        //Config
+        MidnightConfig.init(MOD_ID, RegConfig.class);
+
         RegBlocks.registerBlocks();
+        RegCreativeTabs.init();
     }
 }
