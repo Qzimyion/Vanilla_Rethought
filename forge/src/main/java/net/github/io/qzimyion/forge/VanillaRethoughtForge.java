210f8bdab3e6d0c53ae3a0502206e5e3a1f8815c
Qzimyion <samyakchauhan676@gmail.com>
2024-09-17 00:47:57 +0530

    Restructuring the init forge class

--- forge/src/main/java/net/github/io/qzimyion/forge/VanillaRethoughtForge.java
+++ forge/src/main/java/net/github/io/qzimyion/forge/VanillaRethoughtForge.java
@@ -1,14 +1,19 @@
 package net.github.io.qzimyion.forge;
 
 import dev.architectury.platform.forge.EventBuses;
+import net.github.io.qzimyion.events.ModServerSideEvents;
+import net.minecraft.world.InteractionResult;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import net.github.io.qzimyion.VanillaRethoughtCommon;
 
 @Mod(VanillaRethoughtCommon.MOD_ID)
-public final class ExampleModForge {
-    public ExampleModForge() {
+public final class VanillaRethoughtForge {
+    public VanillaRethoughtForge() {
         // Submit our event bus to let Architectury API register our content on the right time.
         EventBuses.registerModEventBus(VanillaRethoughtCommon.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
 
