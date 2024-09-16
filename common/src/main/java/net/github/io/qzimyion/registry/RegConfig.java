9cc3dd93a7a6332b12b53e67245471d42b3a95e0
Qzimyion <samyakchauhan676@gmail.com>
2024-09-17 00:48:34 +0530

    Added config class

---
+++ common/src/main/java/net/github/io/qzimyion/registry/RegConfig.java
@@ -0,0 +1,9 @@
+package net.github.io.qzimyion.registry;
+
+import eu.midnightdust.lib.config.MidnightConfig;
+
+public class RegConfig extends MidnightConfig {
+
+    @Entry(category = "Events")
+    public static boolean BricksCrackingViaPick = false;
+}
